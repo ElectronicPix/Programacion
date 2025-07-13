7db9f5a13ee6ccf39c2b5341b998d80aa8186f43
ElectronicPix <rubendlpc@gmail.com>
2025-07-12 21:10:07 -0500

    Add initial implementation of main.py with basic object-oriented concepts in Python

---
+++ PythonIA/Módulo de Refuerzo/main.py
@@ -0,0 +1,10 @@
+"""
+    En python, todo es un objeto, y las funciones son objetos de primera clase.
+"""
+numero = 10
+print(type(numero))  # <class 'int'>
+
+#int es un objeto, y numero es una instancia de int
+int = 5
+#string es un objeto, y "hola" es una instancia de strin 
+str = "hola"
